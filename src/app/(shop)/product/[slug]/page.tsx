4d6b97c01837a36d32e44a966e46e7c03da065e4
Sergio Ruiz <sergio.ruiz.teleco@gmail.com>
2025-02-17 12:23:17 +0100

    feat(product): add size selection functionality for clothing and design for product page

--- src/app/(shop)/product/[slug]/page.tsx
+++ src/app/(shop)/product/[slug]/page.tsx
@@ -1,7 +1,51 @@
-export default function Default() {
+import { SizeSelector } from "@/components";
+import { titleFont } from "@/config/fonts";
+import { initialData } from "@/seed/seed";
+import { notFound } from "next/navigation";
+
+interface Props {
+   params: {
+      slug: string
+   }
+}
+
+
+export default function Default({ params }: Props) {
+
+   const { slug } = params;
+   const product = initialData.products.find(product => product.slug === slug);
+
+   if(!product) {
+      notFound()
+   }
+
    return (
-      <div>
-         <h1>Product Page</h1>
+      <div className="mt-5 mb-20 grid md:grid-cols-3 gap-3">
+         {/* Slideshow */}
+
+         <div className="col-span-1 md:col-span-2">
+            <h1>Hola Mundo</h1>
+
+         </div>
+
+         {/* Detalles */}
+         <div className="col-span-1 px-5">
+            <h1 className={`${titleFont.className} antialiased font-bold text-xl`}>{ product.title }</h1>
+            <p className="text-lg mb-5">${ product.price }</p>
+
+            {/* Selector de Tallas */}
+            <SizeSelector selectedSize={product.sizes[1]} availableSizes={product.sizes} />
+
+            {/* Selector de Cantidad */}
+
+            {/* Botón */}
+
+            <button className="btn-primary my-5">Agregar al carrito</button>
+
+            {/* Descripción */}
+            <h3 className="font-bold text-sm">Descripción</h3>
+            <p className="font-light">{ product.description }</p>
+         </div>
       </div>
    );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,6 +2,9 @@ export * from './products/product-grid/ProductGrid';
 export * from './products/product-grid/ProductGridItem';
 
 
+export * from './product/size-selector/SizeSelector';
+
+
 export * from './ui/not-found/PageNotFound';
 export * from './ui/title/Title';
 export * from './ui/top-menu/TopMenu';

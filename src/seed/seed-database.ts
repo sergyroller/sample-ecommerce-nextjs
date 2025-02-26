c4470ea096a29cde3a5210e704059c29fe740858
Sergio Ruiz <sergio.ruiz.teleco@gmail.com>
2025-02-26 09:47:10 +0100

    refactor: Update schema.prisma and seed-database.ts

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -3,13 +3,12 @@ import {initialData} from './seed';
 
 async function main() {
   // 1. Borrar registros previos
-  await Promise.all([
-    prisma.productImage.deleteMany(),
-    prisma.product.deleteMany(),
-    prisma.category.deleteMany(),
-  ]);
 
-  //
+  await prisma.productImage.deleteMany();
+  await prisma.product.deleteMany();
+  await prisma.category.deleteMany();
+
+  // Category Add
 
   const {categories, products} = initialData;
 
@@ -19,6 +18,40 @@ async function main() {
 
   await prisma.category.createMany({
     data: categoriesData,
+  });
+
+  const categoriesDB = await prisma.category.findMany();
+
+  const categoriesMap = categoriesDB.reduce(
+    (map, category) => {
+      map[category.name.toLowerCase()] = category.id;
+
+      return map;
+    },
+    {} as Record<string, string>
+  ); // <string = shirt, string=categoryID>
+
+  // Product Add
+
+  products.forEach(async (product) => {
+    const {type, images, ...rest} = product;
+    const dbProduct = await prisma.product.create({
+      data: {
+        ...rest,
+        categoryId: categoriesMap[type],
+      },
+    });
+
+    // IMAGES
+
+    const imagesData = images.map((image) => ({
+      url: image,
+      productId: dbProduct.id,
+    }));
+
+    await prisma.productImage.createMany({
+      data: imagesData,
+    });
   });
 
   console.log('Seed ejecutaedo correctamente');

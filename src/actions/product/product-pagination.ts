6e3bb22f7ee7c03751d1145d9a075bbb004bd3e3
Sergio Ruiz <sergio.ruiz.teleco@gmail.com>
2025-02-27 00:26:17 +0100

    feat: Add pagination options to product retrieval

--- src/actions/product/product-pagination.ts
+++ src/actions/product/product-pagination.ts
@@ -2,10 +2,23 @@
 
 import prisma from '@/lib/prisma';
 
-export const getPaginatedProductsWithImages = async () => {
+interface PaginationOptions {
+  page?: number;
+  take?: number;
+}
+
+export const getPaginatedProductsWithImages = async ({
+  page = 1,
+  take = 12,
+}: PaginationOptions) => {
+  if (isNaN(Number(page))) page = 1;
+  if (page < 1) page = 1;
+
   try {
+    // 1. Retrieve the products
     const products = await prisma.product.findMany({
-      take: 5,
+      take: take,
+      skip: (page - 1) * take,
       include: {
         ProductImage: {
           take: 2,
@@ -16,11 +29,16 @@ export const getPaginatedProductsWithImages = async () => {
       },
     });
 
-    console.log(products);
+    // 2. Retrieve the total number of products
+    // todo:
+    const totalCounter = await prisma.product.count({});
+    const totalPages = Math.ceil(totalCounter / take);
+
+    /* console.log(products); */
 
     return {
-      currentPage: 1,
-      totalPages: 10,
+      currentPage: page,
+      totalPages: totalPages,
       products: products.map((product) => ({
         ...product,
         images: product.ProductImage.map((image) => image.url),
--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,10 +1,30 @@
+import {redirect} from 'next/navigation';
 import {getPaginatedProductsWithImages} from '@/actions';
 import {ProductGrid, Title} from '@/components';
 
-export default async function Home() {
-  const {products} = await getPaginatedProductsWithImages();
+interface Props {
+  searchParams: {
+    page?: string;
+    take?: string;
+  };
+}
 
-  console.log(products);
+export default async function Home({searchParams}: Props) {
+  // Use 'await' to wait for the promise to resolve.
+
+  // Retrieve the page value, for example: ?page=1.
+  const page = searchParams.page ? parseInt(searchParams.page ?? '') : 1;
+  /* console.log({searchParams}); */
+
+  const {products, currentPage, totalPages} =
+    await getPaginatedProductsWithImages({page});
+
+  console.log(currentPage, totalPages);
+
+  // If products are empty, redirect to the home page.
+  if (products.length === 0) {
+    redirect('/');
+  }
 
   return (
     <>
